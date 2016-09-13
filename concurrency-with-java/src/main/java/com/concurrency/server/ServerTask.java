1c302dd55ba06d0bc125f4218566c65d4d2d560e
lcamilo <lcamilo@esec.com.br>
2016-09-13 00:56:58 -0300

    master: Criando pool de thread fixo

--- concurrency-with-java/src/main/java/com/concurrency/server/ServerTask.java
+++ concurrency-with-java/src/main/java/com/concurrency/server/ServerTask.java
@@ -3,6 +3,8 @@ package com.concurrency.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by luis.camilo on 12/09/2016.
@@ -11,11 +13,13 @@ public class ServerTask {
     public static void main(String... args) throws IOException {
         System.out.println("---- Begin server -----");
         ServerSocket serverSocket = new ServerSocket(12345);
+        ExecutorService threadPool = Executors.newFixedThreadPool(2);
+
         while (true) {
             Socket socket = serverSocket.accept();
             DistribuitorTask distribuitorTask = new DistribuitorTask(socket);
-            Thread thread = new Thread(distribuitorTask);
-            thread.start();
+            threadPool.execute(distribuitorTask);
+
             System.out.println("Accepting new client in port " + socket.getPort());
         }
     }
