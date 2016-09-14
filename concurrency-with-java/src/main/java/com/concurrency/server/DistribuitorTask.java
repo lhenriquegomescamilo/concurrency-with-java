611a81d01781b24d7679847de3c378390c107bf6
lcamilo <lcamilo@esec.com.br>
2016-09-14 00:29:23 -0300

    master: Enviando comando de desligamento para o servidor

--- concurrency-with-java/src/main/java/com/concurrency/server/DistribuitorTask.java
+++ concurrency-with-java/src/main/java/com/concurrency/server/DistribuitorTask.java
@@ -1,6 +1,6 @@
 package com.concurrency.server;
 
-import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -10,21 +10,42 @@ import java.util.Scanner;
 
 public class DistribuitorTask implements Runnable {
     private Socket socket;
+    private ServerTask serverTask;
 
-    public DistribuitorTask(Socket socket) {
+    public DistribuitorTask(Socket socket, ServerTask serverTask) {
         this.socket = socket;
+        this.serverTask = serverTask;
     }
 
     @Override
     public void run() {
         try {
-
             System.out.println("Distributing tasks " + socket);
             Scanner inputClient = new Scanner(socket.getInputStream());
+            PrintStream outPrintStream = new PrintStream(socket.getOutputStream());
+
             while (inputClient.hasNext()) {
                 String command = inputClient.nextLine();
-                System.out.println(command);
+                switch (command) {
+                    case "c1":
+                        outPrintStream.println("Confirmed Command C1");
+                        break;
+                    case "c2":
+                        outPrintStream.println("Confirmed Command C2");
+                        break;
+                    case "c3":
+                        outPrintStream.println("Confirmed Command C3");
+                        break;
+                    case "end":
+                        outPrintStream.println("Sair");
+                        serverTask.stop();
+                        break;
+                    default:
+                        outPrintStream.println("Command not found");
+                }
             }
+            outPrintStream.close();
+            inputClient.close();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
--- concurrency-with-java/src/main/java/com/concurrency/server/ServerTask.java
+++ concurrency-with-java/src/main/java/com/concurrency/server/ServerTask.java
@@ -3,6 +3,7 @@ package com.concurrency.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -10,17 +11,39 @@ import java.util.concurrent.Executors;
  * Created by luis.camilo on 12/09/2016.
  */
 public class ServerTask {
-    public static void main(String... args) throws IOException {
+    private final ServerSocket serverSocket;
+    private final ExecutorService threadPool;
+    private Boolean isRunning;
+
+    ServerTask() throws IOException {
         System.out.println("---- Begin server -----");
-        ServerSocket serverSocket = new ServerSocket(12345);
-        ExecutorService threadPool = Executors.newCachedThreadPool();
+        this.serverSocket = new ServerSocket(12345);
+        this.threadPool = Executors.newCachedThreadPool();
+        this.isRunning = true;
+    }
 
-        while (true) {
-            Socket socket = serverSocket.accept();
-            DistribuitorTask distribuitorTask = new DistribuitorTask(socket);
-            threadPool.execute(distribuitorTask);
-
-            System.out.println("Accepting new client in port " + socket.getPort());
+    public void run() {
+        while (this.isRunning) {
+            try (Socket socket = serverSocket.accept()) {
+                DistribuitorTask distribuitorTask = new DistribuitorTask(socket, this);
+                threadPool.execute(distribuitorTask);
+                System.out.println("Accepting new client in port " + socket.getPort());
+            }catch (Exception e){
+                System.out.println("Server is ruunig ?"+this.isRunning);
+            }
         }
+
+    }
+
+    public void stop() throws IOException {
+        this.isRunning = false;
+        serverSocket.close();
+        threadPool.shutdown();
+    }
+
+    public static void main(String... args) throws IOException {
+        ServerTask server = new ServerTask();
+        server.run();
+        server.stop();
     }
 }
